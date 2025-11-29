220642222e9d2c2d3dba9f5fb30dc64a3078bab5
caidenrev <cimicimicilung4868@gmail.com>
2025-11-29 17:54:27 +0700

    deploy update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin Perpustakaan',
+            'email' => 'admin@geloralibrary.my.id',
+            'password' => bcrypt('password'), // password default
         ]);
     }
 }
